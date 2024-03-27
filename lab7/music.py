b6da3a50c4cdd9b64b6819b0ded378888e392bc5
Nursultan <nu_razak@kbtu.kz>
2024-03-27 14:21:37 +0600

    update 2

--- lab7/music.py
+++ lab7/music.py
@@ -2,15 +2,27 @@ import pygame;
 
 pygame.init();
 
-pygame.mixer.music.load('')
+def make_pL(playList, musicname):
+    playList.append(musicname);
+
+
+pygame.mixer.music.load('music.mp3')
 pygame.mixer.music.play(0);
 
 sc = pygame.display.set_mode((200, 100));
+
+
+pL = [];
+
+make_pL(pL, 'music.mp3')
+make_pL(pL, 'music2.mp3')
+
 
 clock = pygame.time.Clock()
 
 pause = False;
 vol = 1.0;
+i = 0;
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -28,6 +40,22 @@ while True:
             elif event.key == pygame.K_RIGHT:
                 vol += 0.1;
                 pygame.mixer.music.set_volume(vol);
+            elif event.key == pygame.K_z:
+                if i== 0:
+                    i = len(pL) - 1;
+                else:
+                    i -= 1;
+                pygame.mixer.music.load(pL[i]);
+                pygame.mixer.music.play(0);
+            elif event.key == pygame.K_x:
+                if i == len(pL) - 1:
+                    i = 0;
+                else :
+                    i += 1;
+
+                pygame.mixer.music.load(pL[i]);
+                pygame.mixer.music.play(0);
+
 
 
     pygame.display.update();
